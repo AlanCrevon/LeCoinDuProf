65322316226195bdcc29c6b5f21ce5e24d03b5a6
AlanCrevon <alan.crevon@mespreps.com>
2019-05-08 19:41:35 +0200

    Tabs component use css instead of js (#28)

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
@@ -8,20 +7,7 @@ import { NavigationService } from 'src/app/services/navigation.service';
   styleUrls: ['./tabs.page.scss']
 })
 export class TabsPage implements OnInit {
-  public platformWidth: any;
+  constructor(public navigationService: NavigationService) {}
 
-  constructor(public navigationService: NavigationService, public platform: Platform) {}
-
-  ngOnInit() {
-    this.platformWidth = window.innerWidth;
-    this.onResize();
-  }
-
-  onResize() {
-    this.platform.ready().then(() => {
-      this.platform.resize.subscribe(() => {
-        this.platformWidth = window.innerWidth;
-      });
-    });
-  }
+  ngOnInit() {}
 }
