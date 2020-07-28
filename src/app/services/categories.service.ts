eb6ed4d009c213d79178a9e59c54cf03062f82bc
AlanCrevon <alan.crevon@mespreps.com>
2020-07-28 14:42:07 +0200

    Add more items categories

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -7,11 +7,59 @@ export class CategoriesService {
   categories: { id: number; label: string }[] = [
     {
       id: 1,
-      label: 'Livres et manuels'
+      label: 'Livres de jeunesse'
     },
     {
       id: 2,
-      label: 'Mathématiques'
+      label: 'Manuels et guides pédagogiques'
+    },
+    {
+      id: 3,
+      label: 'Manipulation'
+    },
+    {
+      id: 4,
+      label: 'Papéterie'
+    },
+    {
+      id: 5,
+      label: 'Jeux'
+    },
+    {
+      id: 6,
+      label: 'Arts'
+    },
+    {
+      id: 7,
+      label: 'Sciences'
+    },
+    {
+      id: 8,
+      label: 'Arts'
+    },
+    {
+      id: 9,
+      label: 'Sciences'
+    },
+    {
+      id: 10,
+      label: 'Sports'
+    },
+    {
+      id: 11,
+      label: 'Informatique'
+    },
+    {
+      id: 12,
+      label: 'Meubles et rangements'
+    },
+    {
+      id: 13,
+      label: `Jeux d'extérieur`
+    },
+    {
+      id: 14,
+      label: 'Manuels et guides pédagogiques'
     }
   ];
 
