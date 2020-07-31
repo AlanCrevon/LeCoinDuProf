6073a4804d9ee049426b53151ff977dbcf637185
AlanCrevon <alan.crevon@mespreps.com>
2020-07-31 14:12:49 +0200

    Add a message if there are newer shaerd items

--- src/app/pages/tabs/pages/shared/shared.page.ts
+++ src/app/pages/tabs/pages/shared/shared.page.ts
@@ -7,6 +7,8 @@ import { FirestorageService } from 'src/app/modules/upload/services/firestorage.
 import { CategoriesService } from 'src/app/services/categories.service';
 import { ModalController } from '@ionic/angular';
 import { ItemSearchComponent } from '../../components/item-search/item-search.component';
+import { FieldValue } from '@google-cloud/firestore';
+import { firestore } from 'firebase/app';
 
 @Component({
   selector: 'app-shared',
@@ -20,6 +22,8 @@ export class SharedPage implements OnInit {
   loadingMore = false;
   filters;
   itemsPerPage = 12;
+  itemsDisplayedWereCreatedBefore: Date;
+  thereAreNewerItems = false;
 
   constructor(
     private dbService: DbService,
@@ -30,10 +34,65 @@ export class SharedPage implements OnInit {
 
   ngOnInit() {
     this.items$ = this.itemsSubject.asObservable();
-    this.loadSharedItems().subscribe(items => this.itemsSubject.next(items));
+    this.loadSharedItems().subscribe(items => {
+      this.itemsSubject.next(items);
+      this.listenForNewerItems(this.itemsDisplayedWereCreatedBefore).subscribe(newerItems => {
+        this.thereAreNewerItems = newerItems.length > 0;
+      });
+    });
   }
 
-  loadSharedItems(lastVisible?: Item): Observable<Item[]> {
+  listenForNewerItems(newerThan: Date) {
+    return this.dbService
+      .getCollection<Item>('items', (ref: any) => {
+        ref = ref.where('isShared', '==', true).limit(this.itemsPerPage);
+
+        if (!!this.filters) {
+          if (!!this.filters.geohash) {
+            const geohash = this.filters.geohash.substring(0, this.filters.radius);
+            const text = geohash;
+            const end = text.replace(/.$/, (c: string) => String.fromCharCode(c.charCodeAt(0) + 1));
+            ref = ref
+              .where('geohash', '>=', text)
+              .where('geohash', '<', end)
+              .orderBy('geohash', 'asc');
+          }
+          if (!!this.filters.category) {
+            ref = ref.where('category', '==', this.filters.category);
+          }
+        }
+
+        ref = ref.orderBy('createdAt', 'desc').endBefore(newerThan);
+
+        return ref;
+      })
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([] as Item[]);
+        }),
+        map(items => {
+          // If page is incomplete, we reached the end of collection
+          if (items.length < this.itemsPerPage) {
+            this.endReached = true;
+          }
+          return items.map(item => {
+            if (item.hasPicture === true) {
+              // Patch data with observable img
+              item.picture$ = this.firestorageService.download(`/users/${item.owner}/${item.id}/item`);
+            }
+            return item;
+          });
+        })
+      );
+  }
+
+  loadSharedItems(latestCreation?: FieldValue | Date): Observable<Item[]> {
+    if (!!!latestCreation) {
+      latestCreation = new Date();
+      this.itemsDisplayedWereCreatedBefore = latestCreation;
+    }
+
     return this.dbService
       .getCollection<Item>('items', (ref: any) => {
         ref = ref.where('isShared', '==', true).limit(this.itemsPerPage);
@@ -56,7 +115,7 @@ export class SharedPage implements OnInit {
         // Note : if you have a where filter with an inequality
         // (<, <=, >, or >=) on field 'geohash' and so you must also use
         // 'geohash' as your first Query.orderBy()
-        ref = ref.orderBy('createdAt', 'desc').startAfter(!!lastVisible ? lastVisible.createdAt : new Date());
+        ref = ref.orderBy('createdAt', 'desc').startAfter(latestCreation);
         return ref;
       })
       .pipe(
@@ -90,7 +149,7 @@ export class SharedPage implements OnInit {
 
     this.items$ = this.items$.pipe(
       switchMap(displayedItems => {
-        return this.loadSharedItems(displayedItems[displayedItems.length - 1]).pipe(
+        return this.loadSharedItems(displayedItems[displayedItems.length - 1].createdAt).pipe(
           map(newItems => {
             // Remove loading message
             event.target.complete();
@@ -112,6 +171,8 @@ export class SharedPage implements OnInit {
   doRefresh(event?) {
     // Re enable pagination
     this.endReached = false;
+    // Restart from start
+    this.thereAreNewerItems = false;
 
     this.loadSharedItems().subscribe(items => {
       if (!!event) {
